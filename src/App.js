dc7cab73b07329c213ca858c09c87082b386bc5a
Adriano Mota <adrianowsh@hotmail.com>
2019-08-02 00:35:06 -0300

    include drag and drop

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { DndProvider } from 'react-dnd';
+import HTML5Backend from 'react-dnd-html5-backend';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Routes from './routes';
 
@@ -9,14 +11,14 @@ import Board from './components/Board';
 
 function App() {
   return (
-    <>
+    <DndProvider backend={HTML5Backend}>
       <Router>
         <Routes />
         <Global />
         <Header />
         <Board />
       </Router>
-    </>
+    </DndProvider>
   );
 }
 
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import { useDrag } from 'react-dnd';
 import PropTypes from 'prop-types';
 
 import { Container, Label } from './styles';
 import avatar from '../../assets/new_avatar.jpeg';
 
 export default function Card({ data }) {
+  const [{ isDragging }, dragRef] = useDrag({
+    item: { type: 'CARD' },
+    collect: monitor => ({
+      isDragging: monitor.isDragging(),
+    }),
+  });
+
   return (
-    <Container>
+    <Container ref={dragRef} isDragging={isDragging}>
       <header>
         {data.labels.map(label => (
           <Label key={label} color={label} />
